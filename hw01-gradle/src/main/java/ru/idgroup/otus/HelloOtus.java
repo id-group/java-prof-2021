1e1e5ce407a7359e519384bc69073caf61023128
id-group <id-group@yandex.ru>
2022-01-23 20:35:45 +0300

    Подготовка к курсу

---
+++ hw01-gradle/src/main/java/ru/idgroup/otus/HelloOtus.java
@@ -0,0 +1,11 @@
+package ru.idgroup.otus;
+
+import com.google.common.math.BigIntegerMath;
+
+public class HelloOtus {
+
+    public static void main(String... args) {
+        System.out.println(BigIntegerMath.factorial(10));
+    }
+
+}
