982fd5ae9b77b39400227fac959e31f615182e21
id-group <id-group@yandex.ru>
2022-02-06 19:05:54 +0300

    Аннотации правка

--- hw06-annotation/src/main/java/ru/idgroup/otus/annotations/reflection/AnnotationInvoker.java
+++ hw06-annotation/src/main/java/ru/idgroup/otus/annotations/reflection/AnnotationInvoker.java
@@ -14,16 +14,25 @@ public class AnnotationInvoker {
 
     public static void execute(String className) throws ClassNotFoundException {
         Class<?> clazz = Class.forName(className);
-        final Object instantiate = ReflectionHelper.instantiate(clazz);
 
-        //before
-        executeAnnotation(instantiate, Before.class);
+        final Method[] declaredMethods = clazz.getDeclaredMethods();
+        Arrays.stream(declaredMethods)
+                .filter(method -> method.isAnnotationPresent(Test.class))
+                .forEach(method -> {
+                    final Object instantiate = ReflectionHelper.instantiate(clazz);
+                    try {
+                        //before
+                        executeAnnotation(instantiate, Before.class);
+                        ReflectionHelper.callMethod(instantiate, method.getName());
 
-        //test
-        executeAnnotation(instantiate, Test.class);
-
-        //after
-        executeAnnotation(instantiate, After.class);
+                    } catch (Exception e) {
+                        System.out.println("Method:" + method.getName() + " executed with exception:" + e.getMessage());
+                    }
+                    finally {
+                        //after
+                        executeAnnotation(instantiate, After.class);
+                    }
+                });
     }
 
     private static void executeAnnotation(Object invokedObject, Class<? extends Annotation> annotationClass ) {
