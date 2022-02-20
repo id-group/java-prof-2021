e40ba69bcfcd01544689aafd043b64d50401bf43
id-group <id-group@yandex.ru>
2022-02-20 19:38:50 +0300

    Байткод

--- hw10-bc/src/main/java/ru/idgroup/otus/bytecode/demo/TestLoggingImpl.java
+++ hw10-bc/src/main/java/ru/idgroup/otus/bytecode/demo/TestLoggingImpl.java
@@ -7,15 +7,18 @@ public class TestLoggingImpl implements TestLogging{
     @Log
     @Override
     public void calculation(int param1) {
-    }
-
-    //@Log
-    @Override
-    public void calculation(int param1, int param2) {
+        System.out.println(param1);
     }
 
     @Log
     @Override
+    public void calculation(int param1, int param2) {
+        System.out.println(param1 + " " + param2);
+    }
+
+    //@Log
+    @Override
     public void calculation(int param1, int param2, String param3) {
+        System.out.println(param1 + " " + param2 + " " + param3);
     }
 }
--- hw10-bc/src/main/java/ru/idgroup/otus/bytecode/proxy/Ioc.java
+++ hw10-bc/src/main/java/ru/idgroup/otus/bytecode/proxy/Ioc.java
@@ -1,5 +1,6 @@
 package ru.idgroup.otus.bytecode.proxy;
 
+import org.jetbrains.annotations.NotNull;
 import ru.idgroup.otus.bytecode.annotations.Log;
 import ru.idgroup.otus.bytecode.demo.TestLogging;
 import ru.idgroup.otus.bytecode.demo.TestLoggingImpl;
@@ -8,6 +9,10 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Ioc {
 
@@ -16,32 +21,34 @@ public class Ioc {
 
     public static TestLogging createLoggingClass() {
         InvocationHandler handler = new DemoInvocationHandler(new TestLoggingImpl());
-        return (TestLogging) Proxy.newProxyInstance(Ioc.class.getClassLoader(),
-                new Class<?>[]{TestLogging.class}, handler);
+        return (TestLogging) Proxy.newProxyInstance(
+                TestLoggingImpl.class.getClassLoader(),
+                TestLoggingImpl.class.getInterfaces(),
+                handler);
     }
 
     private static class DemoInvocationHandler implements InvocationHandler {
         private final TestLogging myClass;
+        private final Map<String, List<String>> collect;
 
-        DemoInvocationHandler(TestLogging myClass) {
+        DemoInvocationHandler(@NotNull TestLogging myClass) {
             this.myClass = myClass;
+            collect = Arrays.stream(myClass.getClass().getDeclaredMethods())
+                    .filter(method -> method.isAnnotationPresent(Log.class))
+                    .collect(Collectors.groupingBy(Method::getName,
+                            Collectors.mapping(method -> Arrays.stream(method.getParameterTypes()).map(Class::getName).collect(Collectors.joining(",")),
+                            Collectors.toList())));
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
             try {
-
-                //todo без такого преобразования не находит метод с примитивными типами, есть вариант лучше?
-                final Class<?>[] classes = Arrays.stream(args).map(o -> {
-                    if (o.getClass().getSimpleName().equals("Integer"))
-                        return Integer.TYPE;
-                    else
-                        return o.getClass();
-                }).toArray(Class<?>[]::new);
-                var declaredMethod = myClass.getClass().getDeclaredMethod(method.getName(), classes);
-                if (declaredMethod.isAnnotationPresent(Log.class))
-                    System.out.println("executed method: " + declaredMethod.getName() + ", param: " +  Arrays.toString(args));
-
+                if (collect.containsKey(method.getName())) {
+                    List<String> params = collect.get(method.getName());
+                    final String types = Arrays.stream(method.getParameterTypes()).map(Class::getName).collect(Collectors.joining(","));
+                    if(params.contains(types))
+                        System.out.println("executed method: " + method.getName() + ", param: " +  Arrays.toString(args));
+                }
             } catch (Exception ignored) {
             }
             return method.invoke(myClass, args);
