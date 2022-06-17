0db85cd58dd845f18dbbde97090b050c1aa12fd3
id-group <id-group@yandex.ru>
2022-06-17 10:53:45 +0300

    hw31

--- hw31-exec/src/main/java/ru/idgroup/otus/exec/EnumeratorExecutor.java
+++ hw31-exec/src/main/java/ru/idgroup/otus/exec/EnumeratorExecutor.java
@@ -3,6 +3,8 @@ package ru.idgroup.otus.exec;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.CountDownLatch;
+
 public class EnumeratorExecutor {
     private static final Logger logger = LoggerFactory.getLogger(EnumeratorExecutor.class);
     public static final int MAX_COUNT = 10;
@@ -10,74 +12,50 @@ public class EnumeratorExecutor {
     public static final String LAST_COMMAND = "last";
     private static Thread first;
     private static Thread second;
-    private int currentNumber =  1;
-    private int cnt = 0;
-    private static int THREADS_CNT = 2;
+    private Integer currentNumber =  1;
 
     private synchronized void action(String action) {
         while(!Thread.currentThread().isInterrupted()) {
+
             try {
-                for(int i = 1; i <= MAX_COUNT; i++) {
-                    if(cnt==0 && !action.equals(START_COMMAND))
-                        this.wait();
-                    else
-                        this.notifyAll();
-
-                    if (i == currentNumber) {
-                        logger.info(Thread.currentThread().getName() + " i:" + i);
-                        cnt++;
-                        if(cnt==THREADS_CNT && action.equals(LAST_COMMAND)) {
-                            currentNumber++;
-                            cnt=0;
-                            notifyAll();
-                            logger.info("after notify");
-                        }
-                        else
-                            this.wait();
-                    }
-                    else {
-                        this.wait();
-                    }
+                while (currentNumber < MAX_COUNT) {
+                    logger.info(Thread.currentThread().getName() + " i:" + currentNumber);
+                    notifierCommand(action, 1);
+                    this.wait();
                 }
 
-                currentNumber=9;
-                for(int i=MAX_COUNT-1;i>0;i--) {
-                    if(cnt==0 && !action.equals(START_COMMAND))
-                        this.wait();
-                    else
-                        this.notifyAll();
-
-                    if (i == currentNumber) {
-                        logger.info(Thread.currentThread().getName() + " i:" + i);
-                        cnt++;
-                        if(cnt==THREADS_CNT && action.equals(LAST_COMMAND)) {
-                            currentNumber--;
-                            cnt=0;
-                            notifyAll();
-                            logger.info("after notify");
-                        }
-                        else
-                            this.wait();
-                    }
-                    else {
-                        this.wait();
-                    }
+                while (currentNumber > 0) {
+                    logger.info(Thread.currentThread().getName() + " i:" + currentNumber);
+                    notifierCommand(action,-1);
+                    this.wait();
                 }
-                Thread.currentThread().interrupt();
-
-            } catch (InterruptedException ex) {
-                Thread.currentThread().interrupt();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
+            this.notifyAll();
+
+            Thread.currentThread().interrupt();
         }
     }
 
-    public static void main(String[] args) {
+    private void notifierCommand(String action, int inc) {
+        if (action.equals(LAST_COMMAND)) {
+            this.notifyAll();
+            currentNumber = currentNumber + inc;
+        }
+        if (action.equals(START_COMMAND)) {
+            this.notifyAll();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
         EnumeratorExecutor task = new EnumeratorExecutor();
         first = new Thread(() -> task.action(START_COMMAND));
         second = new Thread(() -> task.action(LAST_COMMAND));
 
         first.start();
         second.start();
+
     }
 
     private static void sleep() {
